d6a7e3a307ee1a436b6e5b18bdbcf942e8297801
Lauriethefish <lauriethefish@outlook.com>
2022-07-27 17:16:28 +0100

    Add support for Beat Saver Curator feed (note: no unit tests just yet)

---
+++ SongFeedReaders/Feeds/BeatSaver/FeedSettings/BeatSaverCuratorSettings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SongFeedReaders.Feeds.BeatSaver
+{
+    /// <summary>
+    /// Settings for Beat Saver Curator Recommended feed
+    /// </summary>
+    public class BeatSaverCuratorSettings : BeatSaverFeedSettings, IDatedFeedSettings
+    {
+
+        /// <inheritdoc/>
+        public override string FeedId => "BeatSaver.CuratorRecommended";
+        
+        /// <inheritdoc/>
+        public override int FeedIndex => 0;
+
+        /// <inheritdoc/>
+        public override int SongsPerPage => 20;
+        
+        /// <inheritdoc/>
+        public DateTime StartingDate { get; set; } = DateTime.MinValue;
+        
+        /// <inheritdoc/>
+        public DateTime EndingDate { get; set; } = DateTime.MaxValue;
+
+        /// <inheritdoc/>
+        public override object Clone()
+        {
+            return new BeatSaverMapperSettings()
+            {
+                MaxSongs = this.MaxSongs,
+                Filter = this.Filter,
+                StopWhenAny = this.StopWhenAny,
+                StoreRawData = this.StoreRawData
+            };
+        }
+    }
+}
